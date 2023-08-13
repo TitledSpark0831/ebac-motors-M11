595b43c3636cbf3cfdfc23674530c61f7438188e
TitledSpark0831 <115891695+TitledSpark0831@users.noreply.github.com>
2023-08-13 19:22:08 -0300

    first

---
+++ JS/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $('#carrousel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle();
+    })
+    
+    $('#telefone').mask('(00) 00000-0000', { 
+        /*letra é representada pela letra "S" 
+        numero é representado pelo "0"*/
+        placeholder: '(__) _____-____'
+
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+        },
+
+        messages: {
+            nome: 'Por favor, insira seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numerOfInvalids();
+            console.log(camposIncorretos);
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function(){
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
